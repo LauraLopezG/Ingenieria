7391aba8eb23e0ce1bbadac6994ca859e019a4d2
LauraLopezG <laurasharis14@gmail.com>
2026-02-01 17:18:25 -0500

    fase4

---
+++ frontend/src/app/components/housing-location/housing-location.ts
@@ -0,0 +1,11 @@
+  import { Component, Input } from '@angular/core';
+  import { HousingLocationInterface } from '../housing-location-interface';
+  @Component({
+    selector: 'app-housing-location',
+    imports: [],
+    templateUrl: './housing-location.html',
+    styleUrl: './housing-location.css'
+  })
+  export class HousingLocation {
+    @Input() housingLocation!: HousingLocationInterface;
+  }
